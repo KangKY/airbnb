967c79ad25abb4bb0288648b2659b4deaa942b39
Kang_KI <ruddlf4933@gmail.com>
2021-09-01 17:35:57 +0900

    Remove viewsets

--- rooms/urls.py
+++ rooms/urls.py
@@ -1,17 +1,10 @@
 from django.views.generic import base
-from rest_framework.routers import DefaultRouter
 from django.urls import path
 from . import views
-from .viewsets import RoomViewSet
+
 app_name = 'rooms'
 
-router = DefaultRouter()
-router.register("", RoomViewSet, basename='room')
-
-
-# urlpatterns = [
-#   path("list/", views.ListRoomsView.as_view()),
-#   path("<int:pk>/", views.SeeRoomView.as_view())
-# ]
-
-urlpatterns = router.urls
+urlpatterns = [
+  path("list/", views.ListRoomsView.as_view()),
+  path("<int:pk>/", views.SeeRoomView.as_view())
+]
--- users/serializers.py
+++ users/serializers.py
@@ -2,6 +2,13 @@ from django.db.models import fields
 from rest_framework import serializers
 from .models import User
 
+
+class UserSerializer(serializers.ModelSerializer):
+  class Meta:
+    model = User
+    exclude = ('password', 'user_permissions', 'groups', 'is_active', "favs"
+               "is_staff", "is_superuser", "last_login", "date_joined")
+
 class TinyUserSerializer(serializers.ModelSerializer):
   class Meta:
     model = User
